e445b3e9be69d23657d0897b67bbb64660a14ae8
Kristoffer Erbo Kjær <kristoffer@erbokjaer.dk>
2025-03-11 12:42:47 +0100

    Quick save

--- Assignment_1/functions/predict_m_steps.m
+++ Assignment_1/functions/predict_m_steps.m
@@ -15,9 +15,15 @@ function y_pred = predict_m_steps(A, B, y, u_data, m, N, k)
     u_sampl = [zeros(offset,1);u_data];
     
     % Simulate predictions iteratively
-    for t = numel(B)+max([numel(S),numel(B),numel(R),offset + numel(B)])+k:N + 2 * numel(B) + offset
-        y_pred(t+m-offset) =  R * u_sampl(t+m-numel(B)-k-offset-(0:numel(R)-1)) + S * y_sampl(t-offset-(0:numel(S)-1));
+    for t = numel(B)+max([numel(S),numel(B),numel(R),offset + numel(B)])+k:N + 1 * numel(B) + offset
+        y_pred(t+m-offset) =  R * u_sampl(t+m+(1-numel(B))-k-offset-(1:numel(R))) + S * y_sampl(t-offset-(0:numel(S)-1));
     end
-    y_pred = y_pred(offset+1:N+ offset)';
+    % for t = numel(B)+max([numel(S),numel(B),numel(R),offset + numel(B)])+k:N + 1 * numel(B) + offset
+    %     y_pred(t+m-offset) =  R * u_sampl(t+m-k-offset-(0:numel(R)-1)) + S * y_sampl(t-offset-(0:numel(S)-1));
+    % end
+
+
+
+    y_pred = y_pred(offset + 1:N + offset)';
 end
 
